201e906497af1f709e3fd2d3dd6a0417c3393f03
mreis <mreis@confidencecambio.com.br>
2020-01-29 12:11:09 -0300

    Swagger doc

--- src/main/java/br/com/teste/api/v1/exception/AddressNotFoundAPIException.java
+++ src/main/java/br/com/teste/api/v1/exception/AddressNotFoundAPIException.java
@@ -3,7 +3,7 @@ package br.com.teste.api.v1.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class AddressNotFoundAPIException extends RuntimeException {
 
     public AddressNotFoundAPIException() {
--- src/main/java/br/com/teste/api/v1/rs/AddressRS.java
+++ src/main/java/br/com/teste/api/v1/rs/AddressRS.java
@@ -7,6 +7,8 @@ import br.com.teste.api.v1.output.SuccessCreateDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +29,15 @@ public class AddressRS {
     }
 
     @ApiOperation(value = "Inseri um novo endereço.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Endereço cadastrado com sucesso", response = SuccessCreateDTO.class),
+        @ApiResponse(code = 400, message = "Campo city não informado. |"
+            + " Campo country não informado |"
+            + " Campo neighbourhood não informado |"
+            + " Campo number não informado |"
+            + " Campo state não informado |"
+            + " Campo street_name não informado |"
+            + " Campo zip_code não informado")})
     @RequestMapping(name = "createAddress", consumes = {"application/json"}, method =  RequestMethod.POST)
     public ResponseEntity<SuccessCreateDTO> createAddress(
         @ApiParam(value = "Dados do endereço", required = true)
@@ -36,15 +47,28 @@ public class AddressRS {
     }
 
     @ApiOperation(value = "Recupera um endereco através do Id.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Endereço encontrado", response = GetAddressDTO.class),
+        @ApiResponse(code = 404, message = "Endereço não encontrado")})
     @RequestMapping(name = "getAddress", path = "/{id}", consumes = {"application/json"}, method =
         RequestMethod.GET)
-    public GetAddressDTO getAddress(
+    public ResponseEntity<GetAddressDTO> getAddress(
         @ApiParam(value = "Id do endereço cadastrado", required = true)
         @PathVariable("id") Long id) {
-        return addressService.findById(id);
+        GetAddressDTO getAddressDTO = addressService.findById(id);
+        return new ResponseEntity<>(getAddressDTO, HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "Atualiza um endereco através do Id.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Endereço alterado com sucesso"),
+        @ApiResponse(code = 400, message = "Campo city não informado. |"
+            + " Campo country não informado |"
+            + " Campo neighbourhood não informado |"
+            + " Campo number não informado |"
+            + " Campo state não informado |"
+            + " Campo street_name não informado |"
+            + " Campo zip_code não informado")})
     @RequestMapping(name = "updateAddress", path = "/{id}", consumes = {"application/json"}, method =
         RequestMethod.PUT)
     public ResponseEntity updateAddress(
@@ -57,6 +81,9 @@ public class AddressRS {
     }
 
     @ApiOperation(value = "Exclui um endereco através do Id.")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Endereço excluído com sucesso", response = GetAddressDTO.class),
+        @ApiResponse(code = 404, message = "Endereço não encontrado")})
     @RequestMapping(name = "deleteAddress", path = "/{id}", consumes = {"application/json"}, method =
         RequestMethod.DELETE)
     public ResponseEntity deleteAddress(
